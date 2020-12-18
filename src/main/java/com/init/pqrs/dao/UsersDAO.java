888924aace63a181eb83ea9ee7b61b8e8171a102
Luis Evilla <luisevilla588@gmail.com>
2020-12-18 05:16:55 -0500

    API REST funcional

--- src/main/java/com/init/pqrs/dao/UsersDAO.java
+++ src/main/java/com/init/pqrs/dao/UsersDAO.java
@@ -1,9 +1,16 @@
 package com.init.pqrs.dao;
 
+import java.util.Optional;
+
+import javax.persistence.Entity;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.init.pqrs.entitys.User;
 
 public interface UsersDAO extends JpaRepository<User, Long>{
-		
+		@Query("SELECT u FROM User u WHERE u.email = :email")
+		Optional<User> findByEmail(@Param("email") String email);
 }
--- src/main/java/com/init/pqrs/rest/UsersREST.java
+++ src/main/java/com/init/pqrs/rest/UsersREST.java
@@ -1,10 +1,13 @@
 package com.init.pqrs.rest;
 
+import java.security.SecureRandom;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +15,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.init.pqrs.dao.UsersDAO;
+import com.init.pqrs.entitys.LoginData;
 import com.init.pqrs.entitys.Pqr;
 import com.init.pqrs.entitys.User;
 
@@ -32,21 +37,43 @@ public class UsersREST {
 		return ResponseEntity.ok(users);
 	}
 	
-	@PostMapping
-	public ResponseEntity<User> createPqr(@RequestBody User user) {
+	@RequestMapping(value="/signin", method = RequestMethod.POST)
+	public ResponseEntity<User> signin(@RequestBody LoginData user) {
+		Optional<User> optionalUserDB = usersDAO.findByEmail(user.getEmail());
+		
+		if (optionalUserDB.isPresent()) {
+			User userDB = optionalUserDB.get();
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
+			
+			if (encoder.matches(user.getPassword(), userDB.getPassword())) {
+				return ResponseEntity.ok(userDB);
+			} else {
+				return ResponseEntity.noContent().build();
+			}
+		} else {
+			return ResponseEntity.noContent().build();
+		}
+	}
+	
+	@RequestMapping(value="/signup", method=RequestMethod.POST)
+	public ResponseEntity<User> signup(@RequestBody User user) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
+		
+		String encodedPass = encoder.encode(user.getPassword());
+		user.setPassword(encodedPass);
 		User newUser = usersDAO.save(user);
 		
 		return ResponseEntity.ok(newUser);
 	}
 	
 	@DeleteMapping(value="{userId}")
-	public ResponseEntity<Void> deletePqr(@PathVariable("pqrId") Long userId) {
+	public ResponseEntity<Void> deleteUser(@PathVariable("pqrId") Long userId) {
 		usersDAO.deleteById(userId);
 		return ResponseEntity.ok(null);
 	}
 	
 	@PutMapping
-	public ResponseEntity<User> updatePqr(@RequestBody User user) {
+	public ResponseEntity<User> updateUser(@RequestBody User user) {
 		User updatedUser = usersDAO.save(user);
 		
 		return ResponseEntity.ok(updatedUser);
